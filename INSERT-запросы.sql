fb1d5d3cc790422924bfc174a754005cdcdd9b63
Danil_Dobr <danil.bore@mail.ru>
2025-03-11 17:20:48 +0500

    init

---
+++ INSERT-запросы.sql
@@ -0,0 +1,54 @@
+-- INSERT-запросы (задание 1)
+insert into genres (name)
+values('Поп-жанр'),
+	('R&B'),
+      ('Кантри'),
+      ('Русский шансон');
+
+insert into artists (name)
+values('Пётр Лещенко'), 
+	('Билл Монро'), 
+	('Alicia'), 
+      ('Мартин Гаррикс'); 
+
+insert into albums (name, release_year)
+values('Всё, что было', 1988),
+	('Блюз Собачьего Дома', 2001),
+	('Animals', 2019),
+      ('New Day', 1997);
+
+insert  into tracks (name, duration, album_id )
+values('Осенний мираж', 250, 1),
+	('Я Увидел Свет', 100, 2),
+	('Блюз Скалистой Дороги', 150, 2),
+	('Russian Roulette', 200, 4),
+	('Haegeum my', 350, 3),
+	('Running Wild', 210, 3);
+
+insert into genre_artists (genre_id, artist_id)
+values(1, 1),
+      (2, 2),
+      (3, 3),
+      (4, 4);
+
+insert  into artist_albums (artist_id, album_id)
+values(1, 1),
+      (2, 2),
+      (3, 3),
+      (4, 4);
+
+insert into compilations (title, year)
+values('сборник-1', 2018),
+      ('сборник-2', 2022),
+      ('сборник-3', 2020),
+      ('сборник-4', 2024);
+
+insert  into compilationtracks (compilation_id, track_id)
+values(1, 1),
+      (2, 1),
+      (3, 3),
+      (4, 6);
+
+
+
+
---
+++ SELECT-запросы.sql
@@ -0,0 +1,68 @@
+-- Задание 2
+
+-- Название и продолжительность самого длительного трека.
+select name, duration 
+from tracks t 
+where duration = (select max(duration) from tracks);
+
+-- Название треков, продолжительность которых не менее 3,5 минут.
+select name
+from tracks
+where duration >= 210
+
+-- Названия сборников, вышедших в период с 2018 по 2020 год включительно.
+select title 
+from compilations 
+where year >= 2018 and year <=2020
+
+-- Исполнители, чьё имя состоит из одного слова.
+select name 
+from artists 
+where name NOT LIKE '% %';
+
+-- Название треков, которые содержат слово «мой» или «my».
+select name
+from tracks
+where name like '%my%';
+
+
+-- Задание 3
+-- Количество исполнителей в каждом жанре.
+select g.name, count (ga.artist_id) as artist_count
+from genres g 
+left join genre_artists ga on g.id = ga.genre_id 
+group by g.name 
+
+-- Количество треков, вошедших в альбомы 2019–2020 годов.
+select count(t.id) as track_count
+from tracks t 
+join albums a on t.album_id = a.id  
+where a.release_year  between 2019 and 2020
+
+-- Средняя продолжительность треков по каждому альбому.
+select a.name as album_title, avg(t.duration) as avg_duration 
+from tracks t
+join albums a on t.album_id = a.id 
+group by a.name 
+
+-- Все исполнители, которые не выпустили альбомы в 2020 году.
+SELECT a.name AS artist_name
+FROM artists a
+LEFT JOIN artist_albums aa ON a.id = aa.artist_id
+LEFT JOIN albums al ON aa.album_id = al.id AND al.release_year = 2020
+WHERE al.id IS NULL;
+
+-- Названия сборников, в которых присутствует конкретный исполнитель (Пётр Лещенко).
+SELECT DISTINCT c.title AS collection_title
+FROM compilations c 
+JOIN compilationtracks c2 ON c.id = c2.compilation_id
+JOIN tracks t ON c2.track_id = t.id
+JOIN albums al ON t.album_id = al.id
+JOIN artist_albums aa ON al.id = aa.album_id
+JOIN artists a ON aa.artist_id = a.id
+WHERE a.name = 'Пётр Лещенко';
+
+
+
+
+
